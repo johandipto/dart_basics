1e74a8925525210e41e5878ca04430a4349a5295
diptogazi <johandipto19@gmail.com>
2021-11-27 22:04:51 +0600

    Different kinds of methods containing int, double,string,list ,maps are shown along with two types of arguments.

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,5 +1,49 @@
 void main(List<String> arguments) {
-  var name = 'gazi musa al johan';
+  /*var name = 'gazi musa al johan';
   var user_id = 20;
-  print('Username:$name Userid: $user_id' );
+  print('Username:$name Userid: $user_id' );*/
+  /*int id=10;
+  String name= 'Gazi';*/
+  my_method_name();
+  my_cgpa();
+  String lol= (my_name());
+  print(lol);
+  print (my_map_method());
+
+  /////argument ,,,,argument 2 types -1.normal argument and 2.positional argument
+
+  display_with_normal_argument('this is the kingdom');
+ display_with_positional_argument(yourId: '110', yourName: 'dipto');
+
+}
+
+void display_with_normal_argument(String a){  //here string a is parameter or argument
+print('what is this place ?? $a');
+}
+
+void display_with_positional_argument({String yourId, String yourName}){ //here second bracket is needed to use positional argument
+
+  print('your id is: $yourId , your name is :$yourName');
+}
+
+
+
+
+void my_method_name(){
+ //print ("kaj krtese bro");
+}
+int id(){
+  return 1;
+}
+double my_cgpa(){
+  return 3.3;
+}
+String my_name(){
+  return ' Dipto ';
+}
+List<String> my_list(){
+  return ['name-1', 'name-2'];
+}
+Map<String, String> my_map_method() {
+  return {'key':'value'};
 }
